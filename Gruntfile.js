89ad6f20e9ea9cc1579a122cc32120fabc838406
William Duyck <fuzzyfox0@gmail.com>
2014-03-18 22:47:12 +0000

    adding build option

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,10 +21,36 @@ module.exports = function( grunt ) {
       'Gruntfile.js',
       'asset/js/**/*.js'
       ]
+    },
+
+    concat: {
+      dist: {
+        src: [
+          'asset/js/Array.prototype.shuffle.js',
+          'asset/js/query2object.js',
+          'asset/js/quilt.js'
+        ],
+        dest: 'dist/quilt.js'
+      }
+    },
+
+    uglify: {
+      dist: {
+        options: {
+          mangle: false,
+          sourceMap: true
+        },
+        files: {
+          'dist/quilt.min.js': [ 'dist/quilt.js' ]
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  grunt.loadNpmTasks( 'grunt-contrib-concat' );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 
   grunt.registerTask( 'default', [ 'jshint' ]);
+  grunt.registerTask( 'build', [ 'jshint', 'concat', 'uglify' ]);
 };
