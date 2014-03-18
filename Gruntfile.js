bfa3212a9452bb8e7be3f0b24c5682b094e62f96
William Duyck <fuzzyfox0@gmail.com>
2014-03-18 23:11:37 +0000

    finish build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,13 +44,30 @@ module.exports = function( grunt ) {
           'dist/quilt.min.js': [ 'dist/quilt.js' ]
         }
       }
+    },
+
+    cssmin: {
+      dist: {
+        files: {
+          'dist/quilt.min.css': [ 'asset/css/quilt.css' ]
+        }
+      }
+    },
+
+    copy: {
+      dist: {
+        src: 'asset/css/quilt.css',
+        dest: 'dist/quilt.css'
+      }
     }
   });
 
   grunt.loadNpmTasks( 'grunt-contrib-jshint' );
   grunt.loadNpmTasks( 'grunt-contrib-concat' );
   grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+  grunt.loadNpmTasks( 'grunt-contrib-copy' );
 
   grunt.registerTask( 'default', [ 'jshint' ]);
-  grunt.registerTask( 'build', [ 'jshint', 'concat', 'uglify' ]);
+  grunt.registerTask( 'build', [ 'jshint', 'copy', 'concat', 'uglify', 'cssmin' ]);
 };
